78cde97dca5d7f5e068db645ad3f466a525642f9
Tzeng Yuxio <tzengyuxio@gmail.com>
2012-05-09 17:08:09 +0800

    fix of caption image.

--- plugins/caption_image_tag.rb
+++ plugins/caption_image_tag.rb
@@ -2,7 +2,7 @@
 # Author: Brandon Mathis http://brandonmathis.com
 # Description: Easily output images with optional class names and title/alt attributes
 #
-# Syntax {% image [class name(s)] url [title text] %}
+# Syntax {% caption_img [class name(s)] url [title text] %}
 #
 # Example:
 # {% caption_img left half http://site.com/images/ninja.png Ninja Attack! %}
@@ -16,13 +16,13 @@ module Jekyll
   class CaptionImageTag < Liquid::Tag
     @img = nil
     @title = nil
-    @class = 'caption'
+    @class = ''
     @width = ''
     @height = ''
 
     def initialize(tag_name, markup, tokens)
       if markup =~ /(\S.*\s+)?(https?:\/\/|\/)(\S+)(\s+\d+\s+\d+)?(\s+.+)?/i
-        @class = $1 || 'caption'
+        @class = $1 || ''
         @img = $2 + $3
         if $5
           @title = $5.strip
@@ -38,10 +38,10 @@ module Jekyll
     def render(context)
       output = super
       if @img
-        "<div class='caption-wrapper'>" +
-          "<img class='#{@class.rstrip}' src='#{@img}' width='#{@width}' height='#{@height}' alt='#{@title}' title='#{@title}'>" +
-          "<div class='caption-text'>#{@title}</div>" +
-        "</div>"
+        "<span class='#{('caption-wrapper ' + @class).rstrip}'>" +
+          "<img class='caption' src='#{@img}' width='#{@width}' height='#{@height}' alt='#{@title}' title='#{@title}'>" +
+          "<span class='caption-text'>#{@title}</span>" +
+        "</span>"
       else
         "Error processing input, expected syntax: {% img [class name(s)] /url/to/image [width height] [title text] %}"
       end
